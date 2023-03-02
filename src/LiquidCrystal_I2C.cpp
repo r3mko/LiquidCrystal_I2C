efca3125a23b3e86c7dd228473d29096a475a2e6
Remko Kleinjan <r.a.kleinjan@gmail.com>
2023-03-02 19:03:13 +0100

    Code refactoring Updated keywords Updated library json/properties

--- src/LiquidCrystal_I2C.cpp
+++ src/LiquidCrystal_I2C.cpp
@@ -1,28 +1,6 @@
-// Based on the work by DFRobot
-
 #include "LiquidCrystal_I2C.h"
-#include <inttypes.h>
-#if defined(ARDUINO) && ARDUINO >= 100
 
-#include <Arduino.h>
-
-#define printIIC(args)  Wire.write(args)
-inline size_t LiquidCrystal_I2C::write(uint8_t value) {
-    send(value, Rs);
-    return 1;
-}
-
-#else
-#include <WProgram.h>
-
-#define printIIC(args)  Wire.send(args)
-inline void LiquidCrystal_I2C::write(uint8_t value) {
-    send(value, Rs);
-}
-
-#endif
-#include <Wire.h>
-
+#define printIIC(args) Wire.write(args)
 
 // When the display powers up, it is configured as follows:
 //
@@ -39,12 +17,11 @@ inline void LiquidCrystal_I2C::write(uint8_t value) {
 //    I/D = 1; Increment by 1
 //    S = 0; No shift 
 //
-// Note, however, that resetting the Arduino doesn't reset the LCD, so we
-// can't assume that its in that state when a sketch starts (and the
-// LiquidCrystal constructor is called).
+// Note: however, that resetting the Arduino doesn't reset the LCD,
+// so we can't assume that its in that state when a sketch starts
+// (and the LiquidCrystal constructor is called).
 
-LiquidCrystal_I2C::LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols, uint8_t lcd_rows, uint8_t lcd_sda, uint8_t lcd_scl)
-{
+LiquidCrystal_I2C::LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols, uint8_t lcd_rows, uint8_t lcd_sda, uint8_t lcd_scl) {
     _Addr = lcd_Addr;
     _cols = lcd_cols;
     _rows = lcd_rows;
@@ -53,17 +30,16 @@ LiquidCrystal_I2C::LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols, uint8_t 
     _backlightval = LCD_BACKLIGHT;
 }
 
-void LiquidCrystal_I2C::oled_init(){
+void LiquidCrystal_I2C::init() {
+    init_priv();
+}
+
+void LiquidCrystal_I2C::oled_init() {
     _oled = true;
     init_priv();
 }
 
-void LiquidCrystal_I2C::init(){
-    init_priv();
-}
-
-void LiquidCrystal_I2C::init_priv()
-{
+void LiquidCrystal_I2C::init_priv() {
     if ((_sdaPin != LCD_SDA) && (_sclPin != LCD_SCL)) {
         Wire.begin(_sdaPin, _sclPin);
     }
@@ -80,79 +56,77 @@ void LiquidCrystal_I2C::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
     }
     _numlines = lines;
 
-    // for some 1 line displays you can select a 10 pixel high font
-    if ((dotsize != 0) && (lines == 1)) {
+    // For some 1 line displays you can select a 10 pixel high font
+    if ((dotsize == 10) && (lines == 1)) {
         _displayfunction |= LCD_5x10DOTS;
     }
 
     // SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
-    // according to datasheet, we need at least 40ms after power rises above 2.7V
-    // before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
+    // According to the hitachi HD44780 datasheet, we need at least 40ms after power rises above 2.7V
+    // before sending commands. Arduino can turn on way before 4.5V so we'll wait 50ms
     delay(50); 
   
     // Now we pull both RS and R/W low to begin commands
-    expanderWrite(_backlightval);   // reset expanderand turn backlight off (Bit 8 =1)
-    delay(1000);
+    expanderWrite(_backlightval); // reset expanderand (Bit 8 = 1)
+    delay(500);
 
-    //put the LCD into 4 bit mode
+    // Put the LCD into 4 bit mode
     // this is according to the hitachi HD44780 datasheet
     // figure 24, pg 46
     
-    // we start in 8bit mode, try to set 4 bit mode
+    // We start in 8bit mode
+    // try to set to 4 bit mode
     write4bits(0x03 << 4);
-    delayMicroseconds(4500); // wait min 4.1ms
+    delayMicroseconds(4200); // wait min 4.1ms
    
-    // second try
+    // Second try
     write4bits(0x03 << 4);
-    delayMicroseconds(4500); // wait min 4.1ms
+    delayMicroseconds(110); // wait min 100us
    
-    // third go!
+    // Third time's the charm!
     write4bits(0x03 << 4); 
-    delayMicroseconds(150);
    
-    // finally, set to 4-bit interface
+    // Finally, set to 4-bit interface
     write4bits(0x02 << 4); 
 
-
-    // set # lines, font size, etc.
+    // Set # lines, font size, etc.
     command(LCD_FUNCTIONSET | _displayfunction);  
     
-    // turn the display on with no cursor or blinking default
+    // Turn the display on with no cursor or blinking default
     _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
     display();
     
-    // clear it off
+    // Clear it
     clear();
     
     // Initialize to default text direction (for roman languages)
     _displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
     
-    // set the entry mode
+    // Set the entry mode
     command(LCD_ENTRYMODESET | _displaymode);
     
-    home();
-  
+    home();  
 }
 
 /* High level commands, for the user! */
 
 void LiquidCrystal_I2C::clear(){
-    command(LCD_CLEARDISPLAY);// clear display, set cursor position to zero
-    delayMicroseconds(2000);  // this command takes a long time!
+    command(LCD_CLEARDISPLAY); // Clear display, set cursor position to zero
+    delayMicroseconds(2000); // This command takes a long time!
     if (_oled) {
-        setCursor(0,0);
+        setCursor(0, 0);
     }
 }
 
 void LiquidCrystal_I2C::home(){
-    command(LCD_RETURNHOME);  // set cursor position to zero
-    delayMicroseconds(2000);  // this command takes a long time!
+    command(LCD_RETURNHOME); // Set cursor position to zero
+    delayMicroseconds(2000); // This command takes a long time!
 }
 
 void LiquidCrystal_I2C::setCursor(uint8_t col, uint8_t row){
-    int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
-    if ( row > _numlines ) {
-        row = _numlines-1;    // we count rows starting w/0
+    int row_offsets[] = {0x00, 0x40, 0x14, 0x54};
+    if (row > _numlines) {
+        row = _numlines - 1; // We count rows starting with 0
     }
     command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
 }
@@ -162,6 +136,7 @@ void LiquidCrystal_I2C::noDisplay() {
     _displaycontrol &= ~LCD_DISPLAYON;
     command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
+
 void LiquidCrystal_I2C::display() {
     _displaycontrol |= LCD_DISPLAYON;
     command(LCD_DISPLAYCONTROL | _displaycontrol);
@@ -172,6 +147,7 @@ void LiquidCrystal_I2C::noCursor() {
     _displaycontrol &= ~LCD_CURSORON;
     command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
+
 void LiquidCrystal_I2C::cursor() {
     _displaycontrol |= LCD_CURSORON;
     command(LCD_DISPLAYCONTROL | _displaycontrol);
@@ -182,6 +158,7 @@ void LiquidCrystal_I2C::noBlink() {
     _displaycontrol &= ~LCD_BLINKON;
     command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
+
 void LiquidCrystal_I2C::blink() {
     _displaycontrol |= LCD_BLINKON;
     command(LCD_DISPLAYCONTROL | _displaycontrol);
@@ -191,6 +168,7 @@ void LiquidCrystal_I2C::blink() {
 void LiquidCrystal_I2C::scrollDisplayLeft(void) {
     command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT);
 }
+
 void LiquidCrystal_I2C::scrollDisplayRight(void) {
     command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT);
 }
@@ -219,26 +197,26 @@ void LiquidCrystal_I2C::noAutoscroll(void) {
     command(LCD_ENTRYMODESET | _displaymode);
 }
 
-// Allows us to fill the first 8 CGRAM locations
+// This will fill the first 8 CGRAM locations
 // with custom characters
 void LiquidCrystal_I2C::createChar(uint8_t location, uint8_t charmap[]) {
-    location &= 0x7; // we only have 8 locations 0-7
+    location &= 0x7; // We only have 8 locations 0-7
     command(LCD_SETCGRAMADDR | (location << 3));
     for (int i=0; i<8; i++) {
         write(charmap[i]);
     }
 }
 
-//createChar with PROGMEM input
+// Create Char with PROGMEM input
 void LiquidCrystal_I2C::createChar(uint8_t location, const char *charmap) {
-    location &= 0x7; // we only have 8 locations 0-7
+    location &= 0x7; // We only have 8 locations 0-7
     command(LCD_SETCGRAMADDR | (location << 3));
     for (int i=0; i<8; i++) {
-            write(pgm_read_byte_near(charmap++));
+        write(pgm_read_byte_near(charmap++));
     }
 }
 
-// Turn the (optional) backlight off/on
+// Turn the (optional) backlight on/off
 void LiquidCrystal_I2C::noBacklight(void) {
     _backlightval=LCD_NOBACKLIGHT;
     expanderWrite(0);
@@ -249,21 +227,24 @@ void LiquidCrystal_I2C::backlight(void) {
     expanderWrite(0);
 }
 
-
 /* Mid level commands, for sending data/cmds */
 
 inline void LiquidCrystal_I2C::command(uint8_t value) {
     send(value, 0);
 }
 
+inline size_t LiquidCrystal_I2C::write(uint8_t value) {
+    send(value, Rs);
+    return 1;
+}
 
 /* Low level data pushing commands */
 
-// write either command or data
+// Write either command or data
 void LiquidCrystal_I2C::send(uint8_t value, uint8_t mode) {
     uint8_t highnib=value&0xf0;
     uint8_t lownib=(value<<4)&0xf0;
-       write4bits((highnib)|mode);
+    write4bits((highnib)|mode);
     write4bits((lownib)|mode); 
 }
 
@@ -272,64 +253,62 @@ void LiquidCrystal_I2C::write4bits(uint8_t value) {
     pulseEnable(value);
 }
 
-void LiquidCrystal_I2C::expanderWrite(uint8_t _data){                                        
+void LiquidCrystal_I2C::expanderWrite(uint8_t _data) {                                        
     Wire.beginTransmission(_Addr);
     printIIC((int)(_data) | _backlightval);
     Wire.endTransmission();   
 }
 
-void LiquidCrystal_I2C::pulseEnable(uint8_t _data){
-    expanderWrite(_data | En);  // En high
-    delayMicroseconds(1);       // enable pulse must be >450ns
+void LiquidCrystal_I2C::pulseEnable(uint8_t _data) {
+    expanderWrite(_data | En); // En high
+    delayMicroseconds(1); // Enable pulse must be >450ns
     
     expanderWrite(_data & ~En); // En low
-    delayMicroseconds(50);      // commands need > 37us to settle
+    delayMicroseconds(50); // Commands need > 37us to settle
 } 
 
-
 // Alias functions
-void LiquidCrystal_I2C::cursor_on(){
+void LiquidCrystal_I2C::cursor_on() {
     cursor();
 }
 
-void LiquidCrystal_I2C::cursor_off(){
+void LiquidCrystal_I2C::cursor_off() {
     noCursor();
 }
 
-void LiquidCrystal_I2C::blink_on(){
+void LiquidCrystal_I2C::blink_on() {
     blink();
 }
 
-void LiquidCrystal_I2C::blink_off(){
+void LiquidCrystal_I2C::blink_off() {
     noBlink();
 }
 
-void LiquidCrystal_I2C::load_custom_character(uint8_t char_num, uint8_t *rows){
-        createChar(char_num, rows);
+void LiquidCrystal_I2C::load_custom_character(uint8_t char_num, uint8_t *rows) {
+    createChar(char_num, rows);
 }
 
-void LiquidCrystal_I2C::setBacklight(uint8_t new_val){
-    if(new_val){
-        backlight();        // turn backlight on
-    }else{
-        noBacklight();      // turn backlight off
+void LiquidCrystal_I2C::setBacklight(uint8_t new_val) {
+    if(new_val) {
+        backlight(); // Turn backlight on
+    } else {
+        noBacklight(); // Turn backlight off
     }
 }
 
-void LiquidCrystal_I2C::printstr(const char c[]){
-    //This function is not identical to the function used for "real" I2C displays
-    //it's here so the user sketch doesn't have to be changed 
+void LiquidCrystal_I2C::printstr(const char c[]) {
+    // This function is not identical to the function used for "real" I2C displays
+    // it's here so the user sketch doesn't have to be changed 
     print(c);
 }
 
-
-// unsupported API functions
-void LiquidCrystal_I2C::off(){}
-void LiquidCrystal_I2C::on(){}
-void LiquidCrystal_I2C::setDelay(int cmdDelay,int charDelay) {}
-uint8_t LiquidCrystal_I2C::status(){return 0;}
-uint8_t LiquidCrystal_I2C::keypad(){return 0;}
-uint8_t LiquidCrystal_I2C::init_bargraph(uint8_t graphtype){return 0;}
-void LiquidCrystal_I2C::draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end){}
-void LiquidCrystal_I2C::draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_row_end){}
-void LiquidCrystal_I2C::setContrast(uint8_t new_val){}
+// Unsupported API functions
+void LiquidCrystal_I2C::off() {}
+void LiquidCrystal_I2C::on() {}
+void LiquidCrystal_I2C::setDelay(int cmdDelay, int charDelay) {}
+uint8_t LiquidCrystal_I2C::status() {return 0;}
+uint8_t LiquidCrystal_I2C::keypad() {return 0;}
+uint8_t LiquidCrystal_I2C::init_bargraph(uint8_t graphtype) {return 0;}
+void LiquidCrystal_I2C::draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len, uint8_t pixel_col_end) {}
+void LiquidCrystal_I2C::draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len, uint8_t pixel_row_end) {}
+void LiquidCrystal_I2C::setContrast(uint8_t new_val) {}
--- src/LiquidCrystal_I2C.h
+++ src/LiquidCrystal_I2C.h
@@ -1,11 +1,12 @@
 #ifndef LiquidCrystal_I2C_h
 #define LiquidCrystal_I2C_h
 
+#include <Arduino.h>
 #include <inttypes.h>
 #include <Print.h> 
 #include <Wire.h>
 
-// commands
+// Commands
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x02
 #define LCD_ENTRYMODESET 0x04
@@ -15,13 +16,13 @@
 #define LCD_SETCGRAMADDR 0x40
 #define LCD_SETDDRAMADDR 0x80
 
-// flags for display entry mode
+// Flags for display entry mode
 #define LCD_ENTRYRIGHT 0x00
 #define LCD_ENTRYLEFT 0x02
 #define LCD_ENTRYSHIFTINCREMENT 0x01
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
-// flags for display on/off control
+// Flags for display on/off control
 #define LCD_DISPLAYON 0x04
 #define LCD_DISPLAYOFF 0x00
 #define LCD_CURSORON 0x02
@@ -29,13 +30,13 @@
 #define LCD_BLINKON 0x01
 #define LCD_BLINKOFF 0x00
 
-// flags for display/cursor shift
+// Flags for display/cursor shift
 #define LCD_DISPLAYMOVE 0x08
 #define LCD_CURSORMOVE 0x00
 #define LCD_MOVERIGHT 0x04
 #define LCD_MOVELEFT 0x00
 
-// flags for function set
+// Flags for function set
 #define LCD_8BITMODE 0x10
 #define LCD_4BITMODE 0x00
 #define LCD_2LINE 0x08
@@ -43,13 +44,13 @@
 #define LCD_5x10DOTS 0x04
 #define LCD_5x8DOTS 0x00
 
-// flags for backlight control
+// Flags for backlight control
 #define LCD_BACKLIGHT 0x08
 #define LCD_NOBACKLIGHT 0x00
 
-#define En B00000100  // Enable bit
-#define Rw B00000010  // Read/Write bit
-#define Rs B00000001  // Register select bit
+#define En B00000100 // Enable bit
+#define Rw B00000010 // Read/Write bit
+#define Rs B00000001 // Register select bit
 
 #define LCD_SDA 255
 #define LCD_SCL 255
@@ -57,62 +58,53 @@
 class LiquidCrystal_I2C : public Print {
 public:
   LiquidCrystal_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t lcd_sda = LCD_SDA, uint8_t lcd_scl = LCD_SCL);
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS );
-  void clear();
-  void home();
-  void noDisplay();
-  void display();
-  void noBlink();
-  void blink();
-  void noCursor();
-  void cursor();
-  void scrollDisplayLeft();
-  void scrollDisplayRight();
-  void printLeft();
-  void printRight();
-  void leftToRight();
-  void rightToLeft();
-  void shiftIncrement();
-  void shiftDecrement();
-  void noBacklight();
-  void backlight();
-  void autoscroll();
-  void noAutoscroll(); 
-  void createChar(uint8_t, uint8_t[]);
-  void createChar(uint8_t location, const char *charmap);
-  // Example: 	const char bell[8] PROGMEM = {B00100,B01110,B01110,B01110,B11111,B00000,B00100,B00000};
-  
-  void setCursor(uint8_t, uint8_t); 
-#if defined(ARDUINO) && ARDUINO >= 100
-  virtual size_t write(uint8_t);
-#else
-  virtual void write(uint8_t);
-#endif
-  void command(uint8_t);
   void init();
   void oled_init();
+  void begin(uint8_t cols, uint8_t rows, uint8_t dotsize = LCD_5x8DOTS );
+  void clear();
+  void home();
+  void setCursor(uint8_t, uint8_t); 
+  void noDisplay();
+  void display();
+  void noCursor();
+  void cursor();
+  void noBlink();
+  void blink();
+  void scrollDisplayLeft();
+  void scrollDisplayRight();
+  void leftToRight();
+  void rightToLeft();
+  void autoscroll();
+  void noAutoscroll(); 
+  void createChar(uint8_t location, uint8_t charmap[]);
+  void createChar(uint8_t location, const char *charmap); // Example: const char bell[8] PROGMEM = {B00100,B01110,B01110,B01110,B11111,B00000,B00100,B00000}; 
+  void noBacklight();
+  void backlight();
+  void command(uint8_t);
+  size_t write(uint8_t);
 
-/* compatibility API function aliases */
-void blink_on();						// alias for blink()
-void blink_off();       					// alias for noBlink()
-void cursor_on();      	 					// alias for cursor()
-void cursor_off();      					// alias for noCursor()
-void setBacklight(uint8_t new_val);				// alias for backlight() and nobacklight()
-void load_custom_character(uint8_t char_num, uint8_t *rows);	// alias for createChar()
+/* Compatibility API function aliases */
+
+void cursor_on(); // Alias for cursor()
+void cursor_off(); // Alias for noCursor()
+void blink_on(); // Alias for blink()
+void blink_off(); // Alias for noBlink()
+void load_custom_character(uint8_t char_num, uint8_t *rows); // Alias for createChar()
+void setBacklight(uint8_t new_val); // Alias for backlight() and nobacklight()
 void printstr(const char[]);
 
 /* Unsupported API functions (not implemented in this library) */
-uint8_t status();
-void setContrast(uint8_t new_val);
-uint8_t keypad();
-void setDelay(int,int);
-void on();
+
 void off();
+void on();
+void setDelay(int, int);
+uint8_t status();
+uint8_t keypad();
 uint8_t init_bargraph(uint8_t graphtype);
 void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
 void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
+void setContrast(uint8_t new_val);
 	 
-
 private:
   void init_priv();
   void send(uint8_t, uint8_t);
